2d155e80b99ebefeb6d18969df9ebb49ed610b7c
Murad <mrdaydinov@gmail.com>
2024-04-06 18:15:11 +0400

    inti commit

---
+++ StaticLesson/ArrayExtension.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StaticLesson
+{
+    static class ArrayExtension
+    {
+        public static int[] Add(this int[] inputArray, int inputNum)
+        {
+            int[] outputArray = new int[inputArray.Length + 1];
+            for (int i = 0; i < inputArray.Length; i++)
+            {
+                outputArray[i] = inputArray[i];
+            }
+            outputArray[inputArray.Length] = inputNum;
+            return outputArray;
+        }
+    }
+}
---
+++ StaticLesson/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace StaticLesson
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = new int[0];
+            arr = arr.Add(1);
+            arr = arr.Add(2);
+            arr = arr.Add(3);
+            arr = arr.Add(4);
+            int sum = 0;
+            foreach (var item in arr)
+            {
+                sum += item;
+            }
+            Console.WriteLine(sum);
+        }
+    }
+}
